29b41122ff1892e48caff23960edbc9b16b25d8c
Alexandre Rosenfeld <arsfeld@gmail.com>
2020-02-10 00:07:55 -0500

    [zfs] Add backup via znapzend to data

--- machines/storage/zfs.nix
+++ machines/storage/zfs.nix
@@ -21,18 +21,18 @@ with lib;
     pure = true;
     
     zetup = {
-      "data/homes" = {
+      "data" = {
         # Make snapshots of tank/home every hour, keep those for 1 day,
         # keep every days snapshot for 1 month, etc.
         plan = "1d=>1h,1m=>1d,1y=>1m";
-        recursive = false;
+        recursive = true;
         mbuffer = {
           enable = true;
           port = 17777;
         };
         destinations.local = {
           presend = "zpool import -Nf backup";
-          dataset = "backup/data/homes";
+          dataset = "backup/data";
         };
       };
     };
