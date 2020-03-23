fe3c8de6beda1c5efd96e33d85783acd05cce801
Alexandre Rosenfeld <arsfeld@gmail.com>
2020-03-23 11:09:23 -0400

    Added rclone sync

--- machines/storage/backup.nix
+++ machines/storage/backup.nix
@@ -20,4 +20,24 @@ with lib;
         };
       };
   };
+
+  systemd = {
+    timers.rclone-sync = {
+      wantedBy = [ "timers.target" ];
+      partOf = [ "rclone-sync.service" ];
+      timerConfig.OnCalendar = "daily";
+    };
+    services.rclone-sync = let 
+        rcloneOptions = "--fast-list -P --stats-one-line";
+    in {
+      serviceConfig.Type = "oneshot";
+      serviceConfig.User = "arosenfeld";
+      script = ''
+        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} dropbox: ~/Dropbox
+        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} gdrive: ~/Google\ Drive
+        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} onedrive: ~/One\ Drive
+        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} nextcloud: ~/Nextcloud
+      '';
+    };
+  };
 }
